ac82756246f204769e3565da4eea4c098a01cee2
nka21 <naoki.nico@gmail.com>
2025-08-06 16:55:03 +0900

    feat: ヘッダーの実装
    ボタンの内容は仮

--- apps/extension/src/sidepanel/AppLayout.tsx
+++ apps/extension/src/sidepanel/AppLayout.tsx
@@ -7,7 +7,39 @@ export const AppLayout = (props: AppLayoutProps) => {
 
   return (
     <div className="flex flex-col min-h-full w-full">
-      <header className="h-16 w-full bg-[#151b23]">header</header>
+      <header className="shadow-inner-bottom p-4 w-full bg-[#151b23] gap-3 flex items-center justify-between">
+        <h1 className="text-xl font-bold">Prism</h1>
+        <div className="flex items-center gap-2">
+          {/* TODO: 新しいチャットを開くボタン */}
+          <button
+            className="h-8 w-8 border border-[#3d444d] rounded-md"
+            type="button"
+          >
+            A
+          </button>
+          {/* TODO: チャット履歴を開くボタン */}
+          <button
+            className="h-8 w-8 border border-[#3d444d] rounded-md"
+            type="button"
+          >
+            A
+          </button>
+          {/* TODO: 3点リーダー */}
+          <button
+            className="h-8 w-8 border border-[#3d444d] rounded-md"
+            type="button"
+          >
+            A
+          </button>
+          {/* TODO: 閉じるボタンまたは、ユーザーアイコン ログアウトとか */}
+          <button
+            className="h-8 w-8 border border-[#3d444d] rounded-md"
+            type="button"
+          >
+            A
+          </button>
+        </div>
+      </header>
       <main className="grow">{children}</main>
     </div>
   );
