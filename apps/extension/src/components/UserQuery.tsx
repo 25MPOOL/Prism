25d08d975e2782568a25753e53b64e80a2e0ee20
nka21 <naoki.nico@gmail.com>
2025-08-07 14:53:32 +0900

    fix: classのタイポミスを修正

--- apps/extension/src/components/UserQuery.tsx
+++ apps/extension/src/components/UserQuery.tsx
@@ -9,7 +9,7 @@ export const UserQuery = memo((props: UserQueryProps) => {
 
   return (
     <div className="w-full">
-      <div className="-outline-offseet-1 rounded-md border border-[#3d444d] outline-2 outline-[#316dca]">
+      <div className="-outline-offset-1 rounded-md border border-[#3d444d] outline-2 outline-[#316dca]">
         <div className="rounded-md bg-[#262c36] p-2">
           <p className="whitespace-pre-wrap leading-6">{text}</p>
         </div>
