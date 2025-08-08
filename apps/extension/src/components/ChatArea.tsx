7bee4d2b28314b5185a326689eaaf64deb2ef484
nka21 <naoki.nico@gmail.com>
2025-08-08 18:12:16 +0900

    feat: Geminiとの対話できるように実装

--- apps/extension/src/components/ChatArea.tsx
+++ apps/extension/src/components/ChatArea.tsx
@@ -1,20 +1,21 @@
 import { ModelResponse } from "./ModelResponse";
 import { UserQuery } from "./UserQuery";
+import { useChat } from "@/hooks/useChat";
 
 export const ChatArea = () => {
+  const { messages, isLoading } = useChat();
+
   return (
     <div className="grow overflow-y-auto p-4">
       <div className="flex grow flex-col gap-4">
-        <UserQuery text="こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！" />
-        <ModelResponse text="こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは" />
-        <UserQuery text="こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！" />
-        <ModelResponse text="こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは" />
-        <UserQuery text="こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！" />
-        <ModelResponse text="こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは" />
-        <UserQuery text="こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！" />
-        <ModelResponse text="こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは" />
-        <UserQuery text="こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！こんにちは！" />
-        <ModelResponse text="こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは" />
+        {messages.map((m) =>
+          m.sender === "user" ? (
+            <UserQuery key={m.id} text={m.text} />
+          ) : (
+            <ModelResponse key={m.id} text={m.text} />
+          ),
+        )}
+        {isLoading && <ModelResponse text="Thinking..." />}
       </div>
     </div>
   );
--- apps/extension/src/sidepanel/index.tsx
+++ apps/extension/src/sidepanel/index.tsx
@@ -2,8 +2,18 @@ import { AppLayout } from "@/sidepanel/AppLayout";
 import { ChatInput } from "@/components/ChatInput";
 import { ChatArea } from "@/components/ChatArea";
 import "@/globals.css";
+import { useChat } from "@/hooks/useChat";
+import { useEffect } from "react";
 
 function SidePanel() {
+  const { connect, disconnect } = useChat();
+
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  }, [connect, disconnect]);
+
   return (
     <AppLayout>
       <ChatArea />
