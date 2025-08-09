46efeda434fdfec8b1b2db793ae982644079cdcb
nka21 <naoki.nico@gmail.com>
2025-08-10 01:07:01 +0900

    feat: `/profile` POSTリクエストで、GitHubアイコンを取得

--- apps/extension/src/sidepanel/AppLayout.tsx
+++ apps/extension/src/sidepanel/AppLayout.tsx
@@ -1,12 +1,22 @@
 import { IconClock } from "@/components/ui/IconClock";
 import { IconPlus } from "@/components/ui/IconPlus";
 import { IconShare } from "@/components/ui/IconShare";
+import { useGetProfile } from "@/hooks/api/getProfile";
+import { userKeys } from "@/hooks/api/queryKeys";
+import { useQuery } from "@tanstack/react-query";
+
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
 export const AppLayout = (props: AppLayoutProps) => {
   const { children } = props;
+
+  const { fetchProfile } = useGetProfile();
+  const { data: profile } = useQuery({
+    queryKey: userKeys.profile(),
+    queryFn: () => fetchProfile(),
+  });
 
   return (
     <div className="flex h-full w-full flex-col overflow-hidden">
@@ -36,10 +46,14 @@ export const AppLayout = (props: AppLayoutProps) => {
           </button>
           {/* TODO: 閉じるボタンまたは、ユーザーアイコン ログアウトとか */}
           <button
-            className="h-8 w-8 rounded-md border border-[#3d444d]"
+            className="h-8 w-8 rounded-full border border-[#3d444d]"
             type="button"
           >
-            A
+            <img
+              src={profile?.user.avatarUrl}
+              alt={profile?.user.name}
+              className="h-full w-full rounded-full"
+            />
           </button>
         </div>
       </header>
