44d9450b1d0c0f94c2679e9742bb3e524aa4a622
nka21 <naoki.nico@gmail.com>
2025-08-10 01:09:19 +0900

    コメントの削除

--- apps/extension/src/sidepanel/AppLayout.tsx
+++ apps/extension/src/sidepanel/AppLayout.tsx
@@ -23,28 +23,24 @@ export const AppLayout = (props: AppLayoutProps) => {
       <header className="flex w-full items-center justify-between gap-3 bg-[#151b23] p-4 shadow-inner-bottom">
         <h1 className="font-bold text-xl">Prism</h1>
         <div className="flex items-center gap-2">
-          {/* TODO: 新しいチャットを開くボタン */}
           <button
             className="h-8 w-8 rounded-md border border-[#3d444d]"
             type="button"
           >
             <IconPlus />
           </button>
-          {/* TODO: チャット履歴を開くボタン */}
           <button
             className="h-8 w-8 rounded-md border border-[#3d444d]"
             type="button"
           >
             <IconClock />
           </button>
-          {/* TODO: 3点リーダー */}
           <button
             className="h-8 w-8 rounded-md border border-[#3d444d]"
             type="button"
           >
             <IconShare />
           </button>
-          {/* TODO: 閉じるボタンまたは、ユーザーアイコン ログアウトとか */}
           <button
             className="h-8 w-8 rounded-full border border-[#3d444d]"
             type="button"
